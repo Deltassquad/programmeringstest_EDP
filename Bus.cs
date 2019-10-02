b94c4008dcddc8c1cd4326b846113315e6af4115
Axel Roselius <34334954+Deltassquad@users.noreply.github.com>
2019-10-02 13:16:34 +0200

    late edition
    added an override of ToString for bus and truck classes

--- Bus.cs
+++ Bus.cs
@@ -2,6 +2,9 @@ namespace WindowsApplication1
 {
     public class Bus : Vehicle
     {
-        
-    }
+		public override string ToString()
+		{
+			return Color.Name.ToString() + " Bus " + NrOfPassengers.ToString() + " passangers";
+		}
+	}
 }
--- Truck.cs
+++ Truck.cs
@@ -3,5 +3,11 @@ namespace WindowsApplication1
     public class Truck : Vehicle
     {
 		public int LoadCapacity { get; set; }
+
+		public override string ToString()
+		{
+			return Color.Name.ToString() + " Truck " + LoadCapacity.ToString() + " loadcapacity";
+		}
 	}
+	
 }
